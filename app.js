7ddfbc6ffd9fca122153d88f3e63246928620981
Alba <casadogarcia.alba@jgvaldemora.org>
2026-09-16 13:28:08 +0200

    contador funcionando

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("¡Hola DWEC! Mi taller ya funciona."); 
+const boton = document.getElementById("boton");
+const aviso = document.getElementById("aviso");
+let contador = 0;
+
+boton.addEventListener("click", function () {
+  contador = contador + 1;
+  aviso.textContent = "Has pulsado el botón " + contador + " veces";
+});
