5d849761986749131a56885defaa52708f68a743
Alba <casadogarcia.alba@jgvaldemora.org>
2026-09-16 13:55:22 +0200

    ›La funcionalidad (JavaScript)

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-console.log("¡Hola DWEC! Mi taller ya funciona."); 
+console.log("¡Hola DWEC! Mi taller ya funciona.");
+
 const boton = document.getElementById("boton");
 const aviso = document.getElementById("aviso");
 let contador = 0;
 
-boton.addEventListener("click", function () {
-  contador = contador + 1;
-  aviso.textContent = "Has pulsado el botón " + contador + " veces";
+if (boton && aviso) {
+  boton.addEventListener("click", function () {
+    contador = contador + 1;
+    aviso.textContent = "Has pulsado el botón " + contador + " veces";
+  });
+}
+
+const modonoche = document.getElementById("modonoche");
+
+modonoche.addEventListener("click", function () {
+  document.body.classList.toggle("tema-claro");
+
+  if (document.body.classList.contains("tema-claro")) {
+    modonoche.textContent = "☀️";
+  } else {
+    modonoche.textContent = "🌙";
+  }
 });
