0c722910e192f9b375f26bee227df980c6b56765
Alba <casadogarcia.alba@jgvaldemora.org>
2026-09-24 12:22:30 +0200

    Trabajo 4 completado

---
+++ diario-desarrollador/ejercicios-js/personaje.js
@@ -0,0 +1,22 @@
+// Datos del personaje
+const nombre = "Aragorn";
+let nivel = 5;
+let vida = 100;
+const esJefe = false;
+const habilidades = ["Espada", "Arco", "Liderazgo"];
+
+
+// Mostrar los datos del personaje
+console.log("Nombre: " + nombre);
+console.log("Nivel: " + nivel);
+console.log("Vida: " + vida);
+console.log("¿Es jefe?: " + esJefe);
+console.log("Habilidades: " + habilidades);
+
+
+// Comprobar el tipo de cada variable
+console.log(typeof nombre);
+console.log(typeof nivel);
+console.log(typeof vida);
+console.log(typeof esJefe);
+console.log(typeof habilidades);
