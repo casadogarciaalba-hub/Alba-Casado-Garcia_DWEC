8f216e1537229444be2e87c321b64a9bf72cb237
Alba <casadogarcia.alba@jgvaldemora.org>
2026-09-24 12:18:35 +0200

    Trabajo 3 completado

---
+++ diario-desarrollador/ejercicios-js/bucles.js
@@ -0,0 +1,21 @@
+// 1. Números del 1 al 10
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// 2. Números pares del 2 al 20
+for (let i = 2; i <= 20; i += 2) {
+  console.log(i);
+}
+
+// 3. Recorrer la lista de frutas
+const frutas = ["manzana", "pera", "plátano"];
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+
+// 4. Tabla de multiplicar de un número
+const numero = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(numero + " x " + i + " = " + (numero * i));
+}
